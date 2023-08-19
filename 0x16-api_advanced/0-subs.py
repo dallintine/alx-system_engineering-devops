72e3f0d7c957d1948bddb0ae03772f1ee1d8151d
dallintine <dallintine@yahoo.ca>
2023-08-19 15:31:40 +0000

    work

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
+
 """
 Contains the number_of_subscribers function
 """
 
-=======
-<<<<<<< HEAD
+
 """Function to query subscribers on a given Reddit subreddit."""
->>>>>>> 21c35ece6033d103b3c189a01f16aaaae15e1f54
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-<<<<<<< HEAD
+
     """returns the number of subscribers for a given subreddit"""
     if subreddit is None or type(subreddit) is not str:
         return 0
@@ -21,7 +20,7 @@ def number_of_subscribers(subreddit):
 v1.0.0 (by /u/aaorrico23)'}).json()
     subs = r.get("data", {}).get("subscribers", 0)
     return subs
-=======
+
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
@@ -32,7 +31,7 @@ v1.0.0 (by /u/aaorrico23)'}).json()
         return 0
     results = response.json().get("data")
     return results.get("subscribers")
-=======
+
 """ queries the Reddit API and returns the number of subscribers
  (not active users, total subscribers) for a given subreddit."""
 
@@ -47,5 +46,3 @@ def number_of_subscribers(subreddit):
         return 0
 
     return response.json().get("data").get("subscribers")
->>>>>>> f76b6430d14c5b3cfa515ade96aecd5011262cd8
->>>>>>> 21c35ece6033d103b3c189a01f16aaaae15e1f54
