8d5f919f30effb6aa78a1d4f4b20e3ff98656552
dallintine <dallintine@yahoo.ca>
2023-08-22 17:33:45 +0000

    look okay

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,27 +1,5 @@
-<<<<<<< HEAD
-# Increases the amount of traffic an Nginx server can handle.
-
-# Increase the ULIMIT of the default file
-exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
-} ->
-
-# Restart Nginx
-exec { 'nginx-restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
-=======
-# Fix problem of high amount of requests
-
-exec {'replace':
-  provider => shell,
-  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
-  before   => Exec['restart'],
+# Fix Nginx limits
+exec { 'Limit':
+  command => '/usr/bin/env sed -i s/15/2000/ /etc/default/nginx',
 }
-
-exec {'restart':
-  provider => shell,
-  command  => 'sudo service nginx restart',
->>>>>>> 21c35ece6033d103b3c189a01f16aaaae15e1f54
-}
+exec { '/usr/bin/env service nginx restart': }}
